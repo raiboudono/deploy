5048707c28da921a034ae197f08a92ea18f8fc65
raiboudono <ysann914@gmail.com>
2021-01-06 19:58:18 +0900

    first

---
+++ php/index.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    hello
+
+</body>
+
+</html>
---
+++ php/sample/Sample.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Sample;
+
+
+class Sample
+{
+    public function greet()
+    {
+        echo "raian";
+    }
+}
